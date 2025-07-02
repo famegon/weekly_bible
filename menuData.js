ac86c89bbb6b14cac67e7682bb919455f124166e
famegon <audrhs907@naver.com>
2025-07-02 13:07:37 +0900

    Update menuData.js

--- menuData.js
+++ menuData.js
@@ -1,44 +1,51 @@
 const weeklyBibleStudies = [
+    // 26주차
+    {
+        title: "가짜 기독교와 진짜 기독교",
+        date: "2025년 6월 29일",
+        number: "26th",
+        file: "26-26th.html"
+    },
     // 25주차
-    { 
-        title: "빡세지만 기쁜 신앙생활의 비결", 
+    {
+        title: "빡세지만 기쁜 신앙생활의 비결",
         date: "2025년 6월 22일",
-        number: "25th", 
+        number: "25th",
         file: "25-25th.html"
     },
     // 24주차
-    { 
-        title: "영적으로 자라라", 
+    {
+        title: "영적으로 자라라",
         date: "2025년 6월 15일",
-        number: "24th", 
+        number: "24th",
         file: "25-24th.html"
     },
     // 23주차
-    { 
-        title: "악인이 형통함을 볼 때 어떻게 해야 하나?", 
+    {
+        title: "악인이 형통함을 볼 때 어떻게 해야 하나?",
         date: "2025년 6월 8일",
-        number: "23rd", 
+        number: "23rd",
         file: "25-23rd.html"
     },
     // 22주차
-    { 
-        title: "하나님의 마음에 드는 사람이 되는 데 가장 필요한 것", 
+    {
+        title: "하나님의 마음에 드는 사람이 되는 데 가장 필요한 것",
         date: "2025년 6월 1일",
-        number: "22nd", 
+        number: "22nd",
         file: "25-22nd.html"
     },
     // 21주차
-    { 
-        title: "영원히 행복하게 사는 방법", 
+    {
+        title: "영원히 행복하게 사는 방법",
         date: "2025년 5월 25일",
-        number: "21st", 
-        file: "25-21st.html" 
+        number: "21st",
+        file: "25-21st.html"
     },
     // 20주차
-    { 
-        title: "어떤 스승을 따라야 할까?", 
-        date: "2025년 5월 18일", 
-        number: "20th", 
-        file: "25-20th.html" 
+    {
+        title: "어떤 스승을 따라야 할까?",
+        date: "2025년 5월 18일",
+        number: "20th",
+        file: "25-20th.html"
     }
 ];
